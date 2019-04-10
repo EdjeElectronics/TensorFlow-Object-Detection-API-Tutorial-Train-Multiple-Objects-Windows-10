321b2e9945765e85a03e671dc951fe5283ecae67
Evan <evan.juras@gmail.com>
2019-04-09 18:32:36 -0600

    Reduce min_score_thresh to 0.60

--- Object_detection_image.py
+++ Object_detection_image.py
@@ -107,7 +107,7 @@ vis_util.visualize_boxes_and_labels_on_image_array(
     category_index,
     use_normalized_coordinates=True,
     line_thickness=8,
-    min_score_thresh=0.80)
+    min_score_thresh=0.60)
 
 # All the results have been drawn on image. Now display the image.
 cv2.imshow('Object detector', image)
