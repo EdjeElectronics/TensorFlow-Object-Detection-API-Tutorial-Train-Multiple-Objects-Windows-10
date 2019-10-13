3bf806878ad3c6b3d3ff0df863bd07a67b489309
Evan <evan.juras@gmail.com>
2019-10-12 20:15:08 -0600

    Adjust resizer size to 1/8th instead of 1/4th

--- resizer.py
+++ resizer.py
@@ -17,5 +17,5 @@ for filename in os.listdir(dir_path):
     # If the images are not .JPG images, change the line below to match the image type.
     if filename.endswith(".JPG"):
         image = cv2.imread(filename)
-        resized = cv2.resize(image,None,fx=0.125, fy=0.125, interpolation=cv2.INTER_AREA)
+        resized = cv2.resize(image,None,fx=0.25, fy=0.25, interpolation=cv2.INTER_AREA)
         cv2.imwrite(filename,resized)
